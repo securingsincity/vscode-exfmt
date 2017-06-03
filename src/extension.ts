032fa06b0735218cdb38ec03739d79038ddc1e2d
James Hrisho <james.hrisho@gmail.com>
2017-06-03 12:15:06 -0400

    Working example

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import cp = require('child_process');
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const formatDocument = function(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
+        return new Promise((resolve, reject) => {
+            let filename = document.fileName;
+            var cmd = `mix exfmt ${document.fileName}`;
+            console.log(`cmd line:${cmd}`);
+            const cwd = (vscode.workspace.rootPath) ? vscode.workspace.rootPath : ''
+            cp.exec(cmd, {
+                cwd
+            }, function (error, stdout, stderr) {
 
+                console.log(`stderr: ${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                } else {
+                    let textEdits: vscode.TextEdit[] = [];
+                    let documentEndPosition: vscode.Position =
+                        new vscode.Position(document.lineCount - 1,
+                            document.lineAt(new vscode.Position(document.lineCount - 1, 0)).range.end.character);
+                    let editRange: vscode.Range = new vscode.Range(new vscode.Position(0, 0), documentEndPosition);
+                    textEdits.push(vscode.TextEdit.replace(editRange, stdout));
+                    return resolve(textEdits)
+                }
+            });
+        })
+    }
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "vscode-exfmt" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    let formatter = vscode.languages.registerDocumentFormattingEditProvider('elixir', {
+            provideDocumentFormattingEdits: function(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+                return document.save().then(() => {
+                    return formatDocument(document);
+                });
+            }
+        },
+    );
+    context.subscriptions.push(formatter);
 }
 
 // this method is called when your extension is deactivated
